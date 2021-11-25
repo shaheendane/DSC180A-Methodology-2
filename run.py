2962dfbdd7387b6120afbd42d6090ab98e6c7586
shaheendane <62901626+shaheendane@users.noreply.github.com>
2021-11-25 13:22:42 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,17 @@
 #dont have our actual dataset yet, we have to complete a training in order to access data
-print('hello-world')
+#using first dataset we worked with to do some basic EDA
+#print('hello-world')
+
+import pandas as pd
+import numpy as np
+
+xls = pd.ExcelFile('test/testdata/Smarr PeerJ Data.xlsx')
+femtemp = pd.read_excel(xls, 'FemTemp')
+femact = pd.read_excel(xls, 'FemAct')
+maletemp = pd.read_excel(xls, 'MaleTemp')
+maleact = pd.read_excel(xls, 'MaleAct')
+
+print(femptemp.describe())
+print(femact.describe())
+print(maletemp.describe())
+print(maleact.describe())
